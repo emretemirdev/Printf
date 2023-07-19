8fa0186836a143123a19ed8914930b83c83f5d98
Emre Temir <emre.temir@st.atlas.edu.tr>
2023-07-19 17:59:05 +0300

    inital commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/emre/Masaüstü/printf
+# Install script for directory: /home/emre/Masaüstü/Printf-1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/emre/Masaüstü/printf/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/emre/Masaüstü/Printf-1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
