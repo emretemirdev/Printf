8b2a3cb19010d294ced5a05f6ee22f7b06e1d325
Aliemre44 <emre.temir@st.atlas.edu.tr>
2023-07-24 13:53:22 +0300

    Update Norminette

--- ft_print_addr.c
+++ ft_print_addr.c
@@ -6,21 +6,18 @@
 /*   By: emtemir <emtemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 13:25:55 by emtemir           #+#    #+#             */
-/*   Updated: 2023/07/21 13:27:33 by emtemir          ###   ########.fr       */
+/*   Updated: 2023/07/24 13:02:27 by emtemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//
-// Created by emre on 20.07.2023.
-//
 #include "ft_printf.h"
 
 int	ft_print_addr(unsigned long long n)
 {
-    int	len;
+	int	len;
 
-    len = 0;
-    len += write(1, "0x", 2);
-    len += ft_print_hex(n, 'x');
-    return (len);
+	len = 0;
+	len += write(1, "0x", 2);
+	len += ft_print_hex(n, 'x');
+	return (len);
 }
--- ft_print_hex.c
+++ ft_print_hex.c
@@ -6,40 +6,45 @@
 /*   By: emtemir <emtemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 13:25:58 by emtemir           #+#    #+#             */
-/*   Updated: 2023/07/23 01:02:00 by emre             ###   ########.fr       */
+/*   Updated: 2023/07/24 13:06:11 by emtemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_put_hex(unsigned long long n, char *base) {
-    if (n < 16)
-        ft_putchar(base[n]);
-    else {
-        ft_put_hex(n / 16, base);
-        ft_put_hex(n % 16, base);
-    }
+void	ft_put_hex(unsigned long long n, char *base)
+{
+	if (n < 16)
+		ft_putchar(base[n]);
+	else
+	{
+		ft_put_hex(n / 16, base);
+		ft_put_hex(n % 16, base);
+	}
 }
 
-int	ft_print_hex(unsigned long long n, char format) {
-    char *base_x;
-    char *base_xx;
-    int len;
+int	ft_print_hex(unsigned long long n, char format)
+{
+	char	*base_x;
+	char	*base_xx;
+	int		len;
 
-    base_x = "0123456789abcdef";
-    base_xx = "0123456789ABCDEF";
-    len = 0;
-    if (n == 0)
-        len += write(1, "0", 1);
-    else {
-        if (format == 'x')
-            ft_put_hex(n, base_x);
-        if (format == 'X')
-            ft_put_hex(n, base_xx);
-    }
-    while (n) {
-        n /= 16;
-        ++len;
-    }
-    return (len);
+	base_x = "0123456789abcdef";
+	base_xx = "0123456789ABCDEF";
+	len = 0;
+	if (n == 0)
+		len += write(1, "0", 1);
+	else
+	{
+		if (format == 'x')
+			ft_put_hex(n, base_x);
+		if (format == 'X')
+			ft_put_hex(n, base_xx);
+	}
+	while (n)
+	{
+		n /= 16;
+		++len;
+	}
+	return (len);
 }
